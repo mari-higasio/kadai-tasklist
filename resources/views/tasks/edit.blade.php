05c7094545c6d46ced5de63209404966680cdaba
MariHigashio <kitakantou8000@yahoo.co.jp>
2018-01-17 02:23:27 +0000

    bootstrap end

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,25 @@
 
     <h1>id:{{$task -> id}}のタスク編集ページ</h1>
     
-    {!! Form::model($task,['route'=>['tasks.update',$task->id],'method'=>'put']) !!}
+    <div class="row">
+      <div class="col-xs-6">
     
-      {!! Form::label('status','ステータス') !!}
-      {!! form::text('status') !!}
-      
-      {!! Form::label('content','タスク')!!}
-      {!! Form::text('content')!!}
-      
-      {!! Form::submit('更新') !!}
-      
-    {!!Form::close()!!}
+        {!! Form::model($task,['route'=>['tasks.update',$task->id],'method'=>'put']) !!}
     
+          
+          <div class ="form-group">
+          {!! Form::label('status','ステータス') !!}
+          {!! form::text('status',null,['class'=>'form-control']) !!}
+          </div>
+          <div class ="form-group">
+          {!! Form::label('content','タスク')!!}
+          {!! Form::text('content',null,['class'=>'form-control'])!!}
+          </div>
+          
+          {!! Form::submit('更新',['class'=>'btn btn-info']) !!}
+      
+        {!!Form::close()!!}
+        
+      </div>
+    </div>
 @endsection
